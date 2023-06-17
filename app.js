4f084f6d8392af922272af4121b6a9a84d729b65
ssahibsingh <snone181@gmail.com>
2023-06-17 23:44:14 +0530

    updated

--- app.js
+++ app.js
@@ -1,30 +1,16 @@
-//using selectors inside the element
 const questions = document.querySelectorAll(".question");
 
 questions.forEach(function (question) {
   const btn = question.querySelector(".question-btn");
-  // console.log(btn);
+  const questionText = question.querySelector(".question-text");
 
   btn.addEventListener("click", function () {
-    // console.log(question);
-
+    console.log(btn);
+    questionText.classList.toggle("show-text");
     questions.forEach(function (item) {
       if (item !== question) {
         item.classList.remove("show-text");
       }
     });
-
-    question.classList.toggle("show-text");
   });
 });
-
-// traversing the dom
-// const btns = document.querySelectorAll(".question-btn");
-
-// btns.forEach(function (btn) {
-//   btn.addEventListener("click", function (e) {
-//     const question = e.currentTarget.parentElement.parentElement;
-
-//     question.classList.toggle("show-text");
-//   });
-// });
